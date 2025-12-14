404882487acb96568dd0c49dcf939f507c6da1ba
aya <ayasaber200000@gmail.com>
2025-12-14 15:11:44 +0200

    first js task

---
+++ fifth-task/script.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 100; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 == 0){
+        console.log("Fizz")
+    }
+    else if (i % 5 == 0){
+        console.log("Buzz")
+    }
+    else if (i % 2 == 0){
+        console.log(`Even number: ${i}`)
+    }
+    else {
+        console.log(`Odd number: ${i}`)
+    }
+}
